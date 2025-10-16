d506b42784a3d8de77baedffebd1403d134f2c2b
Jeffrey Chou <jeffreychou1991@gmail.com>
2025-10-15 17:35:24 -0700

    raycast gun

---
+++ Assets/Gun.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Gun : MonoBehaviour
+{
+    public Transform hitSphere;
+    Ray ray;
+    RaycastHit hitInfo;
+    public LayerMask layerMask;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        ray.origin = Camera.main.transform.position;
+        ray.direction = Camera.main.transform.forward;
+        float distance = 100f;
+
+        if(Physics.Raycast(ray, out hitInfo, distance, layerMask))
+        {
+            hitSphere.gameObject.SetActive(true);
+            hitSphere.position = hitInfo.point;
+        }
+        else
+        {
+            hitSphere.gameObject.SetActive(false);
+            hitSphere.position = ray.origin + ray.direction * distance;
+        }
+
+        Debug.DrawLine(ray.origin, hitSphere.position, Color.red, 3f);
+    }
+}
